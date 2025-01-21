b621c63091cc86c90d95a4f134b87d74847f8939
aishwarya988 <singamoniaishwarya@gmail.com>
2025-01-21 15:29:56 +0530

    Add files via upload
    loop program

---
+++ Loop.java
@@ -0,0 +1,15 @@
+public class Loop{
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        System.out.println("HELLO");
+        for(int count = 0; count <=4; count++){
+            System.out.println(count);
+        }
+    }
+}
