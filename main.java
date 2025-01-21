8b07dd99906e365eeb1502ad45d6bd0c04b49fe1
aishwarya988 <singamoniaishwarya@gmail.com>
2025-01-21 15:29:25 +0530

    Add files via upload
    main program

---
+++ main.java
@@ -0,0 +1,9 @@
+class main{
+    public static void main(String[] args) {
+        System.out.println(".HI!\n Good MORNING");
+        System.out.println(".AISHWARYA \n TEJA");
+        System.out.println(9/3);
+        System.out.println(88%99);
+    }
+
+}
