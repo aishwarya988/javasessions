34fbc71ecbf05a824ef64721790701bc5d36864c
aishwarya988 <singamoniaishwarya@gmail.com>
2025-01-21 15:31:52 +0530

    Add files via upload
    problems

---
+++ Day6_Problems.java
@@ -0,0 +1,10 @@
+public class Day6_Problems{
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("a");
+            }
+        System.out.println();
+    }
+}
+}
